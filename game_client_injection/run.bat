43ecdcaa0d309869cfb93a4a303cd53a08068b65
Danny Tuppeny <danny@tuppeny.com>
2018-07-29 11:33:13 +0100

    Tweak run script

--- game_client_injection/run.bat
+++ game_client_injection/run.bat
@@ -1,9 +1,8 @@
 SET VS_TOOLS="C:\Program Files (x86)\Microsoft Visual Studio\Preview\Community\Common7\Tools\VsDevCmd.bat"
 SET GAME_PATH="M:\Games\PwnAdventure3\PwnAdventure3_Data\PwnAdventure3\PwnAdventure3\Binaries\Win32\PwnAdventure3-Win32-Shipping.exe"
 
-pushd "%~dp0"
 call %VS_TOOLS%
-popd
+cd "%~dp0"
 
 lib /def:GameLogic.def /out:GameLogic.lib
 cl /EHsc game_enhancements.cpp /LD /link GameLogic.lib ..\detours\lib.X86\detours.lib
